3bc105ed0a761e2d25246ef7a3403bfdff065bf9
cgt <3498129646@qq.com>
2025-01-15 15:53:38 +0800

    firstCommit

--- src/com/cgt/day1/HelloWorld.java
+++ src/com/cgt/day1/HelloWorld.java
@@ -3,6 +3,7 @@ package com.cgt.day1;
 import org.junit.Test;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("helloworld");
